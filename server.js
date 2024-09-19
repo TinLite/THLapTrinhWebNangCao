55b9fe51010cfa087bc9ea702faa3e658248f8d6
TinLite <37470201+TinLite@users.noreply.github.com>
2024-09-19 09:31:53 +0700

    I.8

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ import express from 'express';
 import dotenv from 'dotenv/config'
 import date from './date';
 import getURL from './getURL';
+import viewEngine from './viewEngine';
 
 const app = express();
 const port = process.env.PORT;
 
+viewEngine(app);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-    res.send('Hello World!. Page about');
+    res.render('about');
 });
 
 app.get('/date', (req, res) => {
     res.send(date());
+});
+
+app.get('/ejs', (req, res) => {
+    res.render('test');
 });
 
 app.get('/geturl', (req, res) => {
